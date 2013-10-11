f3acef460924642e98d0d98731751da8b4ccc1de
vonKristoff <mrjcnicolas@gmail.com>
2013-10-11 14:32:36 +0100

    require js added

--- footer.php
+++ footer.php
@@ -8,6 +8,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
 <script src="js/controls.js"></script>
 <script src="js/init.js"></script>
-
+<!-- require js -->
+<!-- <script data-main="js/main" src="bower_components/requirejs/require.js"></script> -->
 
 </html>
